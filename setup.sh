d001c6107611011f3d53a910dbef11e0a652c91d
Parker Wightman <parkerwightman@gmail.com>
2012-07-25 15:24:31 -0600

    Adding submodule init/update stuff to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 git clone git://github.com/pwightman/config ~/config
+cd ~/config
+git submodule init
+git submodule update
 echo "source ~/config/.vimrc" >> .vimrc
 echo "source ~/config/.bashrc" >> .bashrc
