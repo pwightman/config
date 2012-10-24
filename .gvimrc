106d48b23e7937351446501ef9345ffe4ad58935
Parker Wightman <parkerwightman@gmail.com>
2012-10-23 20:08:08 -0600

    Fixing setup file.

---
+++ .gvimrc
@@ -0,0 +1,11 @@
+set fuopt=maxvert,maxhorz
+if has("gui_running")
+	colorscheme obsidian
+endif
+if has("gui_running")
+	set guioptions=egmrt
+endif
+set columns=150 lines=65
+set transparency=3
+set guifont=Source\ Code\ Pro:h13
+
--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@ git clone git://github.com/pwightman/config ~/config
 cd ~/config
 git submodule init
 git submodule update
-echo "source ~/config/.vimrc" >> .vimrc
-echo "source ~/config/.bashrc" >> .bashrc
+echo "source ~/config/.vimrc" >> ~/.vimrc
+echo "source ~/config/.bashrc" >> ~/.bashrc
+echo "source ~/config/.gvimrc" >> ~/.gvimrc
